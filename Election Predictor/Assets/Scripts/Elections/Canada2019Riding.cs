8373e1d33d8fc9b6e8fcfb86fc68bcad585bf8bc
BHjr132 <bhjr132@gmail.com>
2018-03-29 20:14:24 +1100

    Started Canada 2019 Election

---
+++ Election Predictor/Assets/Scripts/Elections/Canada2019Riding.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canada2019Riding : MonoBehaviour {
+
+    int RidingPopulation;
+
+    float PrevLiberalShareOfVote;
+    float PrevConservativeShareOfVote;
+    float PrevNDPShareOfVote;
+    float PrevGreenShareOfVote;
+    float PrevBlocShareOfVote;
+
+    float NewLiberalShareOfVote;
+    float NewConservativeShareOfVote;
+    float NewNDPShareOfVote;
+    float NewGreenShareOfVote;
+    float NewBlocShareOfVote;
+
+    float LiberalSwing;
+    float ConservativeSwing;
+    float NDPSwing;
+    float GreenSwing;
+    float BlocSwing;
+
+    void UpdateFigures ()
+    {
+        NewLiberalShareOfVote = PrevLiberalShareOfVote + LiberalSwing;
+        NewConservativeShareOfVote = PrevConservativeShareOfVote + ConservativeSwing;
+        NewNDPShareOfVote = PrevNDPShareOfVote + NDPSwing;
+        NewGreenShareOfVote = PrevGreenShareOfVote + GreenSwing;
+        NewBlocShareOfVote = PrevBlocShareOfVote + BlocSwing;
+    }
+
+}
