e1d7378f5a34205fbb02820a30eb6e5e1e9b9ea5
BHjr132 <bhjr132@gmail.com>
2018-04-16 16:40:15 +1000

    Coded UK 2022 Election Predictor

---
+++ Election Predictor/Assets/UK2022/UKEnglandConstituency.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UKEnglandConstituency : MonoBehaviour {
+
+	public enum startingParty {
+		Conservative, Labour, LibDem, UKIP, Green
+	}
+
+	public startingParty StartingParty;
+
+	int ConstituencyPopulation;
+
+	public float PrevConservativeShareOfVote;
+	public float PrevLabourShareOfVote;
+	public float PrevLibDemShareOfVote;
+	public float PrevUKIPShareOfVote;
+	public float PrevGreenShareOfVote;
+
+	float NewConservativeShareOfVote;
+	float NewLabourShareOfVote;
+	float NewLibDemShareOfVote;
+	float NewUKIPShareOfVote;
+	float NewGreenShareOfVote;
+		
+	SpriteRenderer renderer;
+
+	void Start() {
+		renderer = GetComponent<SpriteRenderer> ();
+
+		if (StartingParty == startingParty.Conservative) {
+			renderer.color = new Color32(52, 37, 240, 255);
+		}
+		if (StartingParty == startingParty.Labour) {
+			renderer.color = new Color32(223, 38, 38, 255);
+		}
+		if (StartingParty == startingParty.LibDem) {
+			renderer.color = new Color32(248, 160, 29, 255);
+		}
+		if (StartingParty == startingParty.UKIP) {
+			renderer.color = new Color32(120, 36, 182, 255);
+		}
+		if (StartingParty == startingParty.Green) {
+			renderer.color = new Color32(88, 194, 74, 255);
+		}
+
+	}
+
+	public void UpdateConservativeEnglandFigures (float ConservativeSwing)
+	{
+		NewConservativeShareOfVote = PrevConservativeShareOfVote + ConservativeSwing;
+		if (NewConservativeShareOfVote < 0) {
+			NewConservativeShareOfVote = 0;
+		}
+
+		//Debug.Log ("Con: " + NewConservativeShareOfVote);
+	}
+
+	public void UpdateLabourEnglandFigures (float LabourSwing)
+	{
+		NewLabourShareOfVote = PrevLabourShareOfVote + LabourSwing;
+		if (NewLabourShareOfVote < 0) {
+			NewLabourShareOfVote = 0;
+		}
+
+		//Debug.Log ("Lab: " + NewLabourShareOfVote);
+	}
+
+	public void UpdateLibDemEnglandFigures (float LibDemSwing)
+	{
+		if (PrevLibDemShareOfVote != 0) {
+			NewLibDemShareOfVote = PrevLibDemShareOfVote + LibDemSwing;
+		}
+		if (NewLibDemShareOfVote < 0) {
+			NewLibDemShareOfVote = 0;
+		}
+			
+		//Debug.Log ("Lib Dems: " + NewLibDemShareOfVote);
+	}
+
+	public void UpdateUKIPEnglandFigures (float UKIPSwing)
+	{
+		if (PrevUKIPShareOfVote != 0) {
+			NewUKIPShareOfVote = PrevUKIPShareOfVote + UKIPSwing;
+		}
+		if (NewUKIPShareOfVote < 0) {
+			NewUKIPShareOfVote = 0;
+		}
+			
+		//Debug.Log ("UKIP: " + NewUKIPShareOfVote);
+	}
+
+	public void UpdateGreenEnglandFigures (float GreenSwing)
+	{
+		if (PrevGreenShareOfVote != 0) {
+			NewGreenShareOfVote = PrevGreenShareOfVote + GreenSwing;
+		}
+		if (NewGreenShareOfVote < 0 ) {
+			NewGreenShareOfVote = 0;
+		}
+
+		//Debug.Log ("Green: " + NewGreenShareOfVote);
+	}
+
+	public void UpdateColors () {
+		if (NewConservativeShareOfVote > NewLabourShareOfVote && NewConservativeShareOfVote > NewLibDemShareOfVote && NewConservativeShareOfVote > NewUKIPShareOfVote && NewConservativeShareOfVote > NewGreenShareOfVote) {
+			renderer.color = new Color32(52, 37, 240, 255);
+		} else if(NewLabourShareOfVote > NewLibDemShareOfVote && NewLabourShareOfVote > NewConservativeShareOfVote && NewLabourShareOfVote > NewUKIPShareOfVote && NewLabourShareOfVote > NewGreenShareOfVote) {
+			renderer.color = new Color32(223, 38, 38, 255);
+		} else if(NewLibDemShareOfVote > NewUKIPShareOfVote && NewLibDemShareOfVote > NewConservativeShareOfVote && NewLibDemShareOfVote > NewLabourShareOfVote && NewLibDemShareOfVote > NewGreenShareOfVote) {
+			renderer.color = new Color32(248, 160, 29, 255);
+		} else if (NewUKIPShareOfVote > NewGreenShareOfVote && NewUKIPShareOfVote > NewConservativeShareOfVote && NewUKIPShareOfVote > NewLabourShareOfVote && NewUKIPShareOfVote > NewLibDemShareOfVote) {
+			renderer.color = new Color32(120, 36, 182, 255);
+		} else{
+			renderer.color = new Color32(88, 194, 74, 255);
+		}
+	}
+}
---
+++ Election Predictor/Assets/UK2022/UKSwingCalculator.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UKSwingCalculator : MonoBehaviour {
+
+	[Header("England")]
+	public float EngPrevConservativeShareOfVote;
+	public float EngPrevLabourShareOfVote;
+	public float EngPrevLibDemShareOfVote;
+	public float EngPrevUKIPShareOfVote;
+	public float EngPrevGreenShareOfVote;
+
+	float EngNewConservativeShareOfVote;
+	float EngNewLabourShareOfVote;
+	float EngNewLibDemShareOfVote;
+	float EngNewUKIPShareOfVote;
+	float EngNewGreenShareOfVote;
+
+	float EngConservativeSwing;
+	float EngLabourSwing;
+	float EngLibDemSwing;
+	float EngUKIPSwing;
+	float EngGreenSwing;
+
+	[Header("Scotland")]
+	public float ScotPrevConservativeShareOfVote;
+	public float ScotPrevLabourShareOfVote;
+	public float ScotPrevLibDemShareOfVote;
+	public float ScotPrevUKIPShareOfVote;
+	public float ScotPrevGreenShareOfVote;
+	public float ScotPrevSNPShareOfVote;
+
+	float ScotNewConservativeShareOfVote;
+	float ScotNewLabourShareOfVote;
+	float ScotNewLibDemShareOfVote;
+	float ScotNewUKIPShareOfVote;
+	float ScotNewGreenShareOfVote;
+	float ScotNewSNPShareOfVote;
+
+	float ScotConservativeSwing;
+	float ScotLabourSwing;
+	float ScotLibDemSwing;
+	float ScotUKIPSwing;
+	float ScotGreenSwing;
+	float ScotSNPSwing;
+
+	[Header("Wales")]
+	public float WalesPrevConservativeShareOfVote;
+	public float WalesPrevLabourShareOfVote;
+	public float WalesPrevLibDemShareOfVote;
+	public float WalesPrevUKIPShareOfVote;
+	public float WalesPrevGreenShareOfVote;
+	public float WalesPrevPlaidShareOfVote;
+
+	float WalesNewConservativeShareOfVote;
+	float WalesNewLabourShareOfVote;
+	float WalesNewLibDemShareOfVote;
+	float WalesNewUKIPShareOfVote;
+	float WalesNewGreenShareOfVote;
+	float WalesNewPlaidShareOfVote;
+
+	float WalesConservativeSwing;
+	float WalesLabourSwing;
+	float WalesLibDemSwing;
+	float WalesUKIPSwing;
+	float WalesGreenSwing;
+	float WalesPlaidSwing;
+
+	[Header("UI Objects")]
+	public InputField EngConservativeInput;
+	public InputField EngLabourInput;
+	public InputField EngLibDemInput;
+	public InputField EngUKIPInput;
+	public InputField EngGreenInput;
+
+
+	public void UpdateSwings() {
+		EngNewConservativeShareOfVote = int.Parse (EngConservativeInput.text);
+		EngNewLabourShareOfVote = int.Parse (EngLabourInput.text);
+		EngNewLibDemShareOfVote = int.Parse (EngLibDemInput.text);
+		EngNewUKIPShareOfVote = int.Parse (EngUKIPInput.text);
+		EngNewGreenShareOfVote = int.Parse (EngGreenInput.text);
+
+		//England
+		EngConservativeSwing = EngNewConservativeShareOfVote - EngPrevConservativeShareOfVote;
+		EngLabourSwing = EngNewLabourShareOfVote - EngPrevLabourShareOfVote;
+		EngLibDemSwing = EngNewLibDemShareOfVote - EngPrevLibDemShareOfVote;
+		EngUKIPSwing = EngNewUKIPShareOfVote - EngPrevUKIPShareOfVote;
+		EngGreenSwing = EngNewGreenShareOfVote - EngPrevGreenShareOfVote;
+
+		//Scotland
+		ScotConservativeSwing = ScotNewConservativeShareOfVote - ScotPrevConservativeShareOfVote;
+		ScotLabourSwing = ScotNewLabourShareOfVote - ScotPrevLabourShareOfVote;
+		ScotLibDemSwing = ScotNewLibDemShareOfVote - ScotPrevLibDemShareOfVote;
+		ScotUKIPSwing = ScotNewUKIPShareOfVote - ScotPrevUKIPShareOfVote;
+		ScotGreenSwing = ScotNewGreenShareOfVote - ScotPrevGreenShareOfVote;
+		ScotSNPSwing = ScotNewSNPShareOfVote - ScotPrevSNPShareOfVote;
+
+		//Wales
+		WalesConservativeSwing = WalesNewConservativeShareOfVote - WalesPrevConservativeShareOfVote;
+		WalesLabourSwing = WalesNewLabourShareOfVote - WalesPrevLabourShareOfVote;
+		WalesLibDemSwing = WalesNewLibDemShareOfVote - WalesPrevLibDemShareOfVote;
+		WalesUKIPSwing = WalesNewUKIPShareOfVote - WalesPrevUKIPShareOfVote;
+		WalesGreenSwing = WalesNewGreenShareOfVote - WalesPrevGreenShareOfVote;
+		WalesPlaidSwing = WalesNewPlaidShareOfVote - WalesPrevPlaidShareOfVote;
+
+		gameObject.BroadcastMessage ("UpdateConservativeEnglandFigures", EngConservativeSwing);
+		gameObject.BroadcastMessage ("UpdateLabourEnglandFigures", EngLabourSwing);
+		gameObject.BroadcastMessage ("UpdateLibDemEnglandFigures", EngLibDemSwing);
+		gameObject.BroadcastMessage ("UpdateUKIPEnglandFigures", EngUKIPSwing);
+		gameObject.BroadcastMessage ("UpdateGreenEnglandFigures", EngGreenSwing);
+
+		gameObject.BroadcastMessage ("UpdateColors");
+
+	}
+
+}
